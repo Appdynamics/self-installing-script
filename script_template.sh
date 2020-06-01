6f333c84fd444f0fd4c6703d3ddbd36b3b273023
Diego Pereira <diegopereiraeng@gmail.com>
2020-06-01 19:56:01 -0300

    Suse 11 support

--- script_template.sh
+++ script_template.sh
@@ -34,6 +34,8 @@ remove_pack(){
 
 install(){
         echo "Installing $1..."
+
+
         
         if [ ! -d "$appd_home$1-agent/" ]; then
                 mkdir -p "$appd_home$1-agent/"
@@ -89,7 +91,7 @@ install(){
                 for ((i=0; i < $args_size; i++)){
                         if [ "${args[$i]}" == "-MAContURL" ]; then
                                 echo "Setting controller URL to ${args[$i+1]}"
-                                if [ -f /etc/SuSe-release ]; then
+                                if [ -f /etc/SuSE-release ]; then
                                         cat conf/controller-info.xml | sed "s/(<controller-host>)(.*)(<\/controller-host>)/<controller-host>${args[$i+1]}<\/controller-host>/g" > conf/controller-info.xml.temp
                                 else
                                         cat conf/controller-info.xml | sed -E "s/(<controller-host>)(.*)(<\/controller-host>)/<controller-host>${args[$i+1]}<\/controller-host>/g" > conf/controller-info.xml.temp
@@ -103,7 +105,7 @@ install(){
                                         arg_value=0
                                 fi
                                 echo "Setting controller port to $arg_value"
-                                if [ -f /etc/SuSe-release ]; then
+                                if [ -f /etc/SuSE-release ]; then
                                         cat conf/controller-info.xml | sed "s/(<controller-port>)(.*)(<\/controller-port>)/<controller-port>$arg_value<\/controller-port>/g" > conf/controller-info.xml.temp
                                 else
                                         cat conf/controller-info.xml | sed -E "s/(<controller-port>)(.*)(<\/controller-port>)/<controller-port>$arg_value<\/controller-port>/g" > conf/controller-info.xml.temp
@@ -118,7 +120,7 @@ install(){
                                 fi
                                 if [ $arg_value -eq 1 ]; then
                                         echo "Setting controllerSSL to ${args[$i+1]}"
-                                        if [ -f /etc/SuSe-release ]; then
+                                        if [ -f /etc/SuSE-release ]; then
                                                 cat conf/controller-info.xml | sed "s/(<controller-ssl-enabled>)(true|false)(<\/controller-ssl-enabled>)/<controller-ssl-enabled>true<\/controller-ssl-enabled>/g" > conf/controller-info.xml.temp
                                         else
                                                 cat conf/controller-info.xml | sed -E "s/(<controller-ssl-enabled>)(true|false)(<\/controller-ssl-enabled>)/<controller-ssl-enabled>true<\/controller-ssl-enabled>/g" > conf/controller-info.xml.temp
@@ -127,7 +129,7 @@ install(){
                                 else
                                         if [ $arg_value -eq 0 ]; then
                                                 echo "Setting servervisibility to ${args[$i+1]}"
-                                                if [ -f /etc/SuSe-release ]; then
+                                                if [ -f /etc/SuSE-release ]; then
                                                         cat conf/controller-info.xml | sed "s/(<controller-ssl-enabled>)(true|false)(<\/controller-ssl-enabled>)/<controller-ssl-enabled>false<\/controller-ssl-enabled>/g" > conf/controller-info.xml.temp
                                                 else
                                                         cat conf/controller-info.xml | sed -E "s/(<controller-ssl-enabled>)(true|false)(<\/controller-ssl-enabled>)/<controller-ssl-enabled>false<\/controller-ssl-enabled>/g" > conf/controller-info.xml.temp
@@ -140,7 +142,7 @@ install(){
                         fi
                         if [ "${args[$i]}" == "-MAContAccessKey" ]; then
                                 echo "Setting agent access key to ${args[$i+1]}"
-                                if [ -f /etc/SuSe-release ]; then
+                                if [ -f /etc/SuSE-release ]; then
                                         cat conf/controller-info.xml | sed "s/(<account-access-key>)(.*)(<\/account-access-key>)/<account-access-key>${args[$i+1]}<\/account-access-key>/g" > conf/controller-info.xml.temp
                                 else
                                         cat conf/controller-info.xml | sed -E "s/(<account-access-key>)(.*)(<\/account-access-key>)/<account-access-key>${args[$i+1]}<\/account-access-key>/g" > conf/controller-info.xml.temp
@@ -149,7 +151,7 @@ install(){
                         fi
                         if [ "${args[$i]}" == "-MAContAccount" ]; then
                                 echo "Setting controller account to ${args[$i+1]}"
-                                if [ -f /etc/SuSe-release ]; then
+                                if [ -f /etc/SuSE-release ]; then
                                         cat conf/controller-info.xml | sed "s/(<account-name>)(.*)(<\/account-name>)/<account-name>${args[$i+1]}<\/account-name>/g" > conf/controller-info.xml.temp
                                 else
                                         cat conf/controller-info.xml | sed -E "s/(<account-name>)(.*)(<\/account-name>)/<account-name>${args[$i+1]}<\/account-name>/g" > conf/controller-info.xml.temp
@@ -164,7 +166,7 @@ install(){
                                 fi
                                 if [ $arg_value -eq 1 ]; then
                                         echo "Setting servervisibility to $arg_value"
-                                        if [ -f /etc/SuSe-release ]; then
+                                        if [ -f /etc/SuSE-release ]; then
                                                 cat conf/controller-info.xml | sed "s/(<sim-enabled>)(true|false)(<\/sim-enabled>)/<sim-enabled>true<\/sim-enabled>/g" > conf/controller-info.xml.temp
                                         else
                                                 cat conf/controller-info.xml | sed -E "s/(<sim-enabled>)(true|false)(<\/sim-enabled>)/<sim-enabled>true<\/sim-enabled>/g" > conf/controller-info.xml.temp
@@ -173,7 +175,7 @@ install(){
                                 else
                                         if [ ${args[$i+1]} -eq 0 ]; then
                                                 echo "Setting servervisibility to ${args[$i+1]}"
-                                                if [ -f /etc/SuSe-release ]; then
+                                                if [ -f /etc/SuSE-release ]; then
                                                         cat conf/controller-info.xml | sed "s/(<sim-enabled>)(true|false)(<\/sim-enabled>)/<sim-enabled>false<\/sim-enabled>/g" > conf/controller-info.xml.temp
                                                 else
                                                         cat conf/controller-info.xml | sed -E "s/(<sim-enabled>)(true|false)(<\/sim-enabled>)/<sim-enabled>false<\/sim-enabled>/g" > conf/controller-info.xml.temp
@@ -199,6 +201,28 @@ install(){
                         fi
 
                 }
+                if [ "$ma_user" != "" ]; then
+                        id -u $ma_user > /dev/null
+                        if [ $? -eq 1 ]; then
+                                echo "User $ma_user does not exist, systemd configuration will not continue. Please configure it manually later."
+                                return 1
+                        else
+                                if [ $group_exists -ne 1 ]; then
+                                        echo "Supplied user $ma_user does not belong to the supplied group, aborting systemd configuration. Please configure it manually later."
+                                        return 1
+                                fi
+                                group_exists=`id -gn $ma_user | grep $ma_group | wc -l`
+                        fi
+                else
+                        ma_user="root"
+                fi   
+
+                if [ "$ma_group" == "" ]; then
+                        echo "Group not supplied, defaulting to $ma_user"
+                        ma_group="$ma_user"                       
+                fi
+
+
                 echo "Configuring agent init script..."
                 #echo "MA home: $appd_home$1..."
                 machine_agent_home=`pwd | sed "s/\//\\\\\ \//g"`
@@ -209,25 +233,7 @@ install(){
                         #systemctl exists, so lets use it
                         echo "Unziping Machine Agent unit file..."
                         unzip -q /tmp/agents.zip "MachineAgentTemplate.service"
-			if [ "$ma_user" != "" ]; then
-				id -u $ma_user > /dev/null
-				if [ $? -eq 1 ]; then
-					echo "User $ma_user does not exist, systemd configuration will not continue. Please configure it manually later."
-					return 1
-				else
-					group_exists=`id -gn $ma_user | grep $ma_group | wc -l`
-					if [ $group_exists -ne 1 ]; then
-						echo "Supplied user $ma_user does not belong to the supplied group, aborting systemd configuration. Please configure it manually later."
-						return 1
-					fi
-				fi
-			else
-                        	ma_user="root"
-			fi                        
-                        if [ "$ma_group" == "" ]; then
-				echo "Group not supplied, defaulting to $ma_user"
-                                ma_group="$ma_user"                       
-                        fi
+			                     
                         cat MachineAgentTemplate.service | sed "s/ma_executable/$machine_agent_home\/bin\/machine-agent -d -p $machine_agent_home\/pidfile/g" | sed "s/path_to_pidfile/$machine_agent_home\/pidfile/g" | sed "s/ma_user/$ma_user/g" | sed "s/ma_group/$ma_group/g" > /etc/systemd/system/machine_agent.service
 			echo "Writing unit file machine_agent.service"
 			#cat /etc/systemd/system/machine_agent.service
@@ -240,48 +246,30 @@ install(){
 				return 1
 			fi
                 elif [ -f /sbin/service ]; then
-                        if [ "$ma_user" != "" ]; then
-				id -u $ma_user > /dev/null
-				if [ $? -eq 1 ]; then
-					echo "User $ma_user does not exist, systemd configuration will not continue. Please configure it manually later."
-					return 1
-				else
-					group_exists=`id -gn $ma_user | grep $ma_group | wc -l`
-					if [ $group_exists -ne 1 ]; then
-						echo "Supplied user $ma_user does not belong to the supplied group, aborting systemd configuration. Please configure it manually later."
-						return 1
-					fi
-				fi
-			else
-                        	ma_user="root"
-			fi                        
-                        if [ "$ma_group" == "" ]; then
-				echo "Group not supplied, defaulting to $ma_user"
-                                ma_group="$ma_user"                       
-                        fi
+                                          
+                        
                         echo "Configuring Service /sbin/service"
 
                         echo "Unziping Machine Agent init script..."
                         unzip -q /tmp/agents.zip "appdynamics-machine-agent"
-                        MAInitScript=`cat appdynamics-machine-agent`
-                        if [ "$ma_user" == "" ]; then
-                                $ma_user="root"                        
-                        fi
-                        MAInitScript=`echo $MAInitScript | sed "s/MACHINE_AGENT_HOME=\/opt\/appdynamics\/machine-agent/MACHINE_AGENT_HOME=\"$machine_agent_home\"/g"`
-                        echo $MAInitScript > /etc/init.d/appdynamics-machine-agent
+                        cat appdynamics-machine-agent | sed "s/MACHINE_AGENT_HOME=\/opt\/appdynamics\/machine-agent/MACHINE_AGENT_HOME=\"$machine_agent_home\"/g"  > /etc/init.d/appdynamics-machine-agent
+
+                        #MAInitScript=`echo $MAInitScript | sed "s/MACHINE_AGENT_HOME=\/opt\/appdynamics\/machine-agent/MACHINE_AGENT_HOME=\"$machine_agent_home\"/g"`
+                        #echo $MAInitScript > /etc/init.d/appdynamics-machine-agent
                         chmod +x /etc/init.d/appdynamics-machine-agent
 
                         echo "Configuring the environments for the machine agent"
                         unzip -q /tmp/agents.zip "appdy-sysconfig"
-                        MAConfigScript=`cat appdy-sysconfig`
-                        MAConfigScript=`echo $MAConfigScript | sed "s/MACHINE_AGENT_USER=changeit/MACHINE_AGENT_USER=\"$ma_user\"/g"`
-                        MAConfigScript=`echo $MAConfigScript | sed "s/MACHINE_AGENT_GROUP=changeit/MACHINE_AGENT_GROUP=\"$ma_group\"/g"`
-                        MAConfigScript=`echo $MAConfigScript | sed "s/MACHINE_AGENT_HOME=\/opt\/appdynamics\/machine-agent/MACHINE_AGENT_HOME=\"$machine_agent_home\"/g"`
-                        MAConfigScript=`echo $MAConfigScript | sed "s/JAVA_HOME=\/opt\/appdynamics\/machine-agent\/jre/JAVA_HOME=\"$machine_agent_home\/jre\"/g"`
-                        echo $MAConfigScript > /etc/sysconfig/appdynamics-machine-agent
+                        
+                        cat appdy-sysconfig | sed "s/MACHINE_AGENT_USER=changeit/MACHINE_AGENT_USER=\"$ma_user\"/g"` | sed "s/MACHINE_AGENT_GROUP=changeit/MACHINE_AGENT_GROUP=\"$ma_group\"/g" | sed "s/MACHINE_AGENT_HOME=\/opt\/appdynamics\/machine-agent/MACHINE_AGENT_HOME=\"$machine_agent_home\"/g" | sed "s/JAVA_HOME=\/opt\/appdynamics\/machine-agent\/jre/JAVA_HOME=\"$machine_agent_home\/jre\"/g" > /etc/sysconfig/appdynamics-machine-agent
+
+
                         chmod +x /etc/sysconfig/appdynamics-machine-agent
+
                         chkconfig appdynamics-machine-agent on
+
                         echo "Success configuring machine-agent service"
+
                 else
                         which "chkconfig"
                         if [ $? -eq 0 ]; then
@@ -289,9 +277,7 @@ install(){
                                 echo "Unziping Machine Agent init script..."
                                 unzip -q /tmp/agents.zip "ma_init.sh"
                                 MAInitScript=`cat ma_init.sh`
-                                if [ "$ma_user" == "" ]; then
-                                        $ma_user="root"                        
-                                fi
+
                                 MAInitScript=`echo $MAInitScript | sed "s/USER=\"\"/USER=\"$ma_user\"/g"`
                                 MAInitScript=`echo $MAInitScript | sed "s/AGENT_HOME=\"\"/AGENT_HOME=\"$machine_agent_home\"/g"`
                                 echo $MAInitScript > /etc/init.d/machine_agent
@@ -302,9 +288,7 @@ install(){
                                 if [ $? -eq 0 ]; then
                                         #update-rc.d exists, so lets use it
                                         MAInitScript=`cat ma_init.sh`
-                                        if [ "$ma_user" == "" ]; then
-                                                $ma_user="root"                        
-                                        fi
+
                                         MAInitScript=`echo $MAInitScript | sed "s/USER=\"\"/USER=\"$ma_user\"/g"`
                                         MAInitScript=`echo $MAInitScript | sed "s/AGENT_HOME=\"\"/AGENT_HOME=\"$machine_agent_home\"/g"`
                                         echo $MAInitScript > /etc/init.d/machine_agent
@@ -326,7 +310,9 @@ install(){
                 unzip -q $zip
                 rm -rf $zip
                 sudo ./install.sh
+                chown -R $ma_user:$ma_group "$appd_home$1-agent/"
         fi
+        
         
         #echo "tail -n +$((archive + 1)) ../$0 > /tmp/agents.zip"
 
